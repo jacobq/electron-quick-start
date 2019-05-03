7105c9e87cc219683f9e74ecb73223d2820aa6cd
Jacob Quant <jacobq@gmail.com>
2019-05-03 13:04:43 -0500

    EXAMPLE: call show/hide/remove from renderer

--- renderer.js
+++ renderer.js
@@ -1,3 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { remote } = require('electron');
+const mainWindow = remote.getCurrentWindow();
+
+function toggleMenuBarVisibility(shouldShow) {
+  mainWindow.setMenuBarVisibility(shouldShow);
+}
+
+function removeMenu() {
+  if (typeof mainWindow.removeMenu === 'function') {
+    mainWindow.removeMenu();
+  }
+  else {
+    console.warn('Sorry, this version of electron does not appear to support this API');
+  }
+}
+
+document.querySelector('button#show').addEventListener('click', e => toggleMenuBarVisibility(true));
+document.querySelector('button#hide').addEventListener('click', e => toggleMenuBarVisibility(false));
+document.querySelector('button#remove').addEventListener('click', removeMenu);
