18593a5069d689277d3ad9325f8e9333e7bf360b
Jacob Quant <jacobq@gmail.com>
2019-10-30 11:14:08 -0500

    feat: try 8.0.0-beta.1
    Note: had to enable `webPreferences.sandbox = true` for this to work.

--- main.js
+++ main.js
@@ -25,7 +25,8 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      sandbox: true,
     }
   })
 
