bf1d024ed604d2cceef4c39cf7597399edf0b648
Jacob Quant <jacobq@gmail.com>
2019-10-31 16:33:43 -0500

    revert: go back to electron 7.0.0
    Revert "feat: try 8.0.0-beta.1"
    This reverts commit 18593a5069d689277d3ad9325f8e9333e7bf360b.

--- main.js
+++ main.js
@@ -25,8 +25,7 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      sandbox: true,
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
